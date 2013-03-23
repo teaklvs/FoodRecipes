47dc0412713f29a2daec0120fcd4752c1113d1d9
Dragana <draganagligorovska@live.com>
2013-03-24 00:39:33 +0100

    button

--- FoodRecipes/src/com/example/foodrecipes/MainActivity.java
+++ FoodRecipes/src/com/example/foodrecipes/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.foodrecipes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE1 = "com.example.foodrecipes.MESSAGE1";
@@ -20,6 +22,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void sendMessage(View view) {
+	    Intent intent = new Intent(this, PrebarajActivity.class);
+	    startActivity(intent);
 	}
 
 }

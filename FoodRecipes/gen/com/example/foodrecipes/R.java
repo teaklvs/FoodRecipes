5dedf3627e0f783e110663b6368c1a56109d5abc
Dragana <draganagligorovska@live.com>
2013-03-23 23:19:13 +0100

    Search

--- FoodRecipes/gen/com/example/foodrecipes/R.java
+++ FoodRecipes/gen/com/example/foodrecipes/R.java
@@ -43,14 +43,15 @@ public final class R {
         public static final int main=0x7f070002;
     }
     public static final class string {
-        public static final int Prebaraj=0x7f050006;
+        public static final int Prebaraj=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int ime=0x7f050003;
-        public static final int recept=0x7f050005;
-        public static final int title_activity_how_to_cook=0x7f050007;
-        public static final int vreme=0x7f050004;
+        public static final int btnSearch=0x7f050007;
+        public static final int hello_world=0x7f050008;
+        public static final int ime=0x7f050002;
+        public static final int recept=0x7f050004;
+        public static final int title_activity_how_to_cook=0x7f050006;
+        public static final int vreme=0x7f050003;
     }
     public static final class style {
         /** 

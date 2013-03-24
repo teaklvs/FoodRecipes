30f8ca3af3655f9196637c301073b31418409be5
Dragana <draganagligorovska@live.com>
2013-03-24 21:12:29 +0100

    promena

--- FoodRecipes/src/com/example/foodrecipes/MainActivity.java
+++ FoodRecipes/src/com/example/foodrecipes/MainActivity.java
@@ -1,56 +1,51 @@
 package com.example.foodrecipes;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
+//import android.view.Menu;
 import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+public class MainActivity extends Activity {
+	
 
-public class MainActivity extends ListActivity {
 	public final static String EXTRA_MESSAGE1 = "com.example.foodrecipes.MESSAGE1";
 	public final static String EXTRA_MESSAGE2 = "com.example.foodrecipes.MESSAGE2";
 	public final static String EXTRA_MESSAGE3 = "com.example.foodrecipes.MESSAGE3";
-
-	String className[] = { "EasyChocolateCake", "ChocolateCupcakesAndPeanutButterIcing", "Pancakes"};
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
-		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, className));
-				
-	}
-
-	@Override
-	protected void onListItemClick(ListView lv,View v, int position, long id)
-	{
-		super.onListItemClick(lv,v,position,id);
-		String openClass = className[position];
-		try
-		{
-			Class selected = Class.forName("com.example.foodrecipes."+ openClass);
-			Intent selectedIntent = new Intent(this,selected);
-			startActivity(selectedIntent);
-		}
-		catch(ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
+	public final static String EXTRA_MESSAGE4 = "com.example.foodrecipes.MESSAGE4";
+	
+	ListView listView;
 	
 	@Override
-	protected void onPause()
-	{
-		super.onPause();
-		finish();
+	protected void onCreate(Bundle savedInstanceState) {	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ListView listView = (ListView) findViewById(R.id.mylist);
+		String[] values = new String[] { "Easy chocolate cake", "Chocolate Cupcakes and Peanut Butter Icing", "Pancakes"};
+		
+		// Define a new Adapter
+		// First parameter - Context
+		// Second parameter - Layout for the row
+		// Third parameter - ID of the TextView to which the data is written
+		// Forth - the Array of data
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		listView.setAdapter(adapter);
+		
+		//listView.setOnItemSelectedListener(new selectedFunction());
+		
+		
 	}
 	
 	public void sendMessage(View view) {
 	    Intent intent = new Intent(this, PrebarajActivity.class);
 	    startActivity(intent);
 	}
-}
 
+	
+	
+}

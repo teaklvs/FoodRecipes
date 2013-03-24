c9781e986e978a71a9f5b5f72b30320abcdac6ad
Irina Veljanoska <irinaveljanoska@yahoo.com>
2013-03-24 10:17:37 +0100

    PrebarajOnCreate

--- FoodRecipes/src/com/example/foodrecipes/PrebarajActivity.java
+++ FoodRecipes/src/com/example/foodrecipes/PrebarajActivity.java
@@ -1,7 +1,14 @@
 package com.example.foodrecipes;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class PrebarajActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_prebaraj);
+	}
 
 }

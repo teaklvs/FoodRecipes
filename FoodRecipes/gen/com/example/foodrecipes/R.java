7af678a0b536de01d7cf3042af4eb0bf541fc95b
Dragana <draganagligorovska@live.com>
2013-03-24 18:29:38 +0100

    listView

--- FoodRecipes/gen/com/example/foodrecipes/R.java
+++ FoodRecipes/gen/com/example/foodrecipes/R.java
@@ -28,15 +28,16 @@ public final class R {
         public static final int off=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
+        public static final int action_settings=0x7f090009;
         public static final int btnBack=0x7f090004;
-        public static final int btnBackRecipes=0x7f090007;
+        public static final int btnBackRecipes=0x7f090008;
         public static final int imageView1=0x7f090000;
-        public static final int selSpinner=0x7f090005;
+        public static final int mylist=0x7f090005;
+        public static final int selSpinner=0x7f090006;
         public static final int textView1=0x7f090002;
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090003;
-        public static final int txtSpinner=0x7f090006;
+        public static final int txtSpinner=0x7f090007;
     }
     public static final class layout {
         public static final int activity_how_to_cook=0x7f030000;
--- FoodRecipes/src/com/example/foodrecipes/MainActivity.java
+++ FoodRecipes/src/com/example/foodrecipes/MainActivity.java
@@ -1,20 +1,72 @@
 package com.example.foodrecipes;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
 	public final static String EXTRA_MESSAGE1 = "com.example.foodrecipes.MESSAGE1";
 	public final static String EXTRA_MESSAGE2 = "com.example.foodrecipes.MESSAGE2";
 	public final static String EXTRA_MESSAGE3 = "com.example.foodrecipes.MESSAGE3";
+	public final static String EXTRA_MESSAGE4 = "com.example.foodrecipes.MESSAGE4";
+	
+	ListView listView;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView listView = (ListView) findViewById(R.id.mylist);
+		String[] values = new String[] { "Easy chocolate cake", "Chocolate Cupcakes and Peanut Butter Icing", "Pancakes"};
+		
+		// Define a new Adapter
+		// First parameter - Context
+		// Second parameter - Layout for the row
+		// Third parameter - ID of the TextView to which the data is written
+		// Forth - the Array of data
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		listView.setAdapter(adapter);
+		
+		listView.setOnItemSelectedListener(new selectedFunction());
+	}
+
+	public class selectedFunction implements OnItemSelectedListener {
+		
+		@Override
+		public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		
+			// TODO Auto-generated method stub			
+			String selectedNumber = "";
+			if(arg2==0)
+			{
+				selectedNumber="0";
+			}
+			else if(arg2==1)
+			{
+				selectedNumber="1";
+			}
+			else if(arg2==2)
+			{
+				selectedNumber="2";
+			}
+			Intent intent = new Intent(this, HowToCook.class);
+			intent.putExtra(EXTRA_MESSAGE4, selectedNumber);
+			
+			startActivity(intent);
+		}
+			
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+		}
 	}
 
 	@Override
